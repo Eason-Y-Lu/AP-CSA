7a22f02cb14a88828b26ca1f8ac64c0649a6e1dc
Eason Lu <aelnosu@outlook.com>
2024-01-18 15:52:29 -0800

    Finish Fast-Expy
    Signed-off-by: Eason Lu <aelnosu@outlook.com>

--- fast-expt/src/Main.java
+++ fast-expt/src/Main.java
@@ -1,10 +1,23 @@
+//code by Om
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(fastExpt(7, 10));
     }
 
-    public static int expt(int base, int power) {
-
-        return 0;
+    public static int fastExpt(int b, int n) {
+        int i = n;
+        int m = 1;
+        while (i > 0) {
+            if (i % 2 == 0) {
+                b *= b;
+                i /= 2;
+            } else {
+                m *= b;
+                i -= 1;
+            }
+        }
+        return m;
     }
+
 }
+
